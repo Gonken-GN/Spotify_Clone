17a3afb46970c4fb0140a96fe504e3ee8a893100
Fadhil <fadhilanugrah21@gmail.com>
2023-06-29 17:34:58 +0700

    first commit

---
+++ components/Box.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+interface boxProps{
+    children: React.ReactNode;
+    className?: string,
+}
+
+const Box: React.FC<boxProps> = ({
+    children,
+    className
+}) => {
+    return ( <div className={twMerge(`bg-neutral-900 rounded-lg h-fit w-full`, className)}>
+        {children}
+    </div> );
+}
+ 
+export default Box;
