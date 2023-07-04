e89372ebb439122c3a9349bda211c9bed48103ca
Fadhil <fadhilanugrah21@gmail.com>
2023-07-04 23:56:54 +0700

    auth login

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
 interface HeaderProps {
   children: React.ReactNode;
   classname?: string;
@@ -13,6 +14,8 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ children, classname }) => {
   const router = useRouter();
   const logOut = () => {};
+  const authModal = useAuthModal();
+
   return (
     <div
       className={twMerge(
@@ -46,10 +49,17 @@ const Header: React.FC<HeaderProps> = ({ children, classname }) => {
         <div className="flex justify-between items-center gap-x-4">
           <>
             <div>
-              <Button onClick={() => {}} className="bg-transparent text-neutral-300 font-medium">Sign Up</Button>
+              <Button
+                onClick={authModal.onOpen}
+                className="bg-transparent text-neutral-300 font-medium"
+              >
+                Sign Up
+              </Button>
             </div>
             <div>
-              <Button onClick={() => {}} className="bg-white px-6 py-2">Log In</Button>
+              <Button onClick={authModal.onOpen} className="bg-white px-6 py-2">
+                Log In
+              </Button>
             </div>
           </>
         </div>
