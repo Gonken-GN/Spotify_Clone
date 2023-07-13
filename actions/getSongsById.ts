c4cf6bdd8295bf50d0536deccaa5f12676d54996
Fadhil <fadhilanugrah21@gmail.com>
2023-07-14 00:38:01 +0700

    add like function 14.7.2023

--- actions/getSongsById.ts
+++ actions/getSongsById.ts
@@ -7,14 +7,19 @@ const getSongsById = async (): Promise<Song[]> => {
   const supabase = createServerComponentClient({
     cookies: cookies,
   });
-  const { data: sessionData, error: sessionError} = await supabase.auth.getSession();
+  const { data: sessionData, error: sessionError } =
+    await supabase.auth.getSession();
 
-  if(sessionError) {
+  if (sessionError) {
     console.log(sessionError.message);
     return [];
   }
-  const { data, error } = await supabase.from("songs").select("*").eq('user_id', sessionData.session?.user.id).order('created_at', {ascending: false});
-  if(error) {
+  const { data, error } = await supabase
+    .from("songs")
+    .select("*")
+    .eq("user_id", sessionData.session?.user.id)
+    .order("created_at", { ascending: false });
+  if (error) {
     console.log(error.message);
     return [];
   }
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -11,10 +11,15 @@ export default async function Home() {
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
         </div>
-        <div 
-        className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4
-        gap-3 mt-4">
-          <ListItem image="/images/liked.png" name="Liked Songs" href="liked"></ListItem>
+        <div
+          className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4
+        gap-3 mt-4"
+        >
+          <ListItem
+            image="/images/liked.png"
+            name="Liked Songs"
+            href="liked"
+          ></ListItem>
         </div>
       </Header>
       <div className="mt-2 mb-7 px-6">
@@ -24,7 +29,7 @@ export default async function Home() {
         <div>List of Songs</div>
       </div>
       <div>
-        <PageContent songs={songs}/>
+        <PageContent songs={songs} />
       </div>
     </div>
   );
