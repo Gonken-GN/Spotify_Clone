ca9f8b40c3a200f3f819e7ef77db38a7fad2e7e8
Fadhil <fadhilanugrah21@gmail.com>
2023-07-21 21:23:51 +0700

    add minor player function

---
+++ hooks/useGetSongById.tsx
@@ -0,0 +1,80 @@
+import { Song } from "@/types";
+import { useSessionContext } from "@supabase/auth-helpers-react";
+import { useEffect, useMemo, useState } from "react";
+import toast from "react-hot-toast";
+
+const useGetSongById = async (id?: string) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [song, setSong] = useState<Song | undefined>(undefined);
+  const { supabaseClient } = useSessionContext();
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    setIsLoading(true);
+    const fetchSong = async () => {
+      const { data, error } = await supabaseClient
+        .from("songs")
+        .select("*")
+        .eq("id", id)
+        .single();
+
+      if (error) {
+        toast.error(error.message);
+      }
+      setSong(data as Song);
+      setIsLoading(false);
+    };
+    fetchSong();
+  }, [id, supabaseClient]);
+  
+  return useMemo(() => ({ isLoading, song }), [isLoading, song]);
+};
+export default useGetSongById;
+
+/* 
+import { useEffect, useMemo, useState } from "react";
+import { toast } from "react-hot-toast";
+import { useSessionContext } from "@supabase/auth-helpers-react";
+
+import { Song } from "@/types";
+
+const useSongById = (id?: string) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [song, setSong] = useState<Song | undefined>(undefined);
+  const { supabaseClient } = useSessionContext();
+
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    const fetchSong = async () => {
+      const { data, error } = await supabaseClient
+        .from('songs')
+        .select('*')
+        .eq('id', id)
+        .single();
+
+      if (error) {
+        setIsLoading(false);
+        return toast.error(error.message);
+      }
+      
+      setSong(data as Song);
+      setIsLoading(false);
+    }
+
+    fetchSong();
+  }, [id, supabaseClient]);
+
+  return useMemo(() => ({
+    isLoading,
+    song
+  }), [isLoading, song]);
+};
+
+export default useSongById;
+*/
